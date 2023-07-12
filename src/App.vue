781bdfd579f495e1ba3ed4539de0e2889af95253
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-07-12 21:42:00 +0800

    feat:配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script lang="ts" setup>
+import request from "@/utils/request.ts"
 
+async function getData() {
+  const res = await request.get('/home/index')
+  console.log(res)
+}
+
+getData()
 </script>
 
 <template>
