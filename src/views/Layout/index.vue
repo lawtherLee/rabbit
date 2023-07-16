6ba5b44a4153145eed7adb93fedf03cec0142e2d
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-07-16 23:21:30 +0800

    feat:layput底部

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,12 @@
 <script lang='ts' name="layout" setup>
 import AppTopNav from "./components/app-top-nav.vue";
 import AppHeader from "@/views/Layout/components/app-header.vue";
+import AppFooter from "@/views/Layout/components/app-footer.vue";
 </script>
 <template>
   <app-top-nav/>
   <app-header/>
+  <app-footer/>
 </template>
 
 <style>
