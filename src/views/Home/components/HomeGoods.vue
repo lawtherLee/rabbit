d2e30b0a630f476548dfa9ce1a4fc1abaa405b3c
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-07-26 21:53:49 +0800

    style:配置加载过度动画

--- src/views/Home/components/HomeGoods.vue
+++ src/views/Home/components/HomeGoods.vue
@@ -1,16 +1,30 @@
 <template>
-  <ul class="goods-list">
-    <li v-for="item in goods" :key="item.id">
-      <RouterLink to="/">
-        <img v-lazy="item.picture" alt="" />
-        <p class="name ellipsis">{{ item.name }}</p>
-        <p class="price">
-          <!--          作用域插槽-->
-          <slot :row="item"></slot>
-        </p>
-      </RouterLink>
-    </li>
-  </ul>
+  <transition>
+    <ul v-if="goods.length" class="goods-list">
+      <li v-for="item in goods" :key="item.id">
+        <RouterLink to="/">
+          <img v-lazy="item.picture" alt="" />
+          <p class="name ellipsis">{{ item.name }}</p>
+          <p class="price">
+            <!--          作用域插槽-->
+            <slot :row="item"></slot>
+          </p>
+        </RouterLink>
+      </li>
+    </ul>
+    <div v-else class="home-skeleton">
+      <div
+        v-for="i in 4"
+        :key="i"
+        :style="{ backgroundColor: '#f0f9f4' }"
+        class="item"
+      >
+        <XtxSkeleton :height="306" :width="306" animated bg="#e4e4e4" />
+        <XtxSkeleton :height="24" :width="160" animated bg="#e4e4e4" />
+        <XtxSkeleton :height="24" :width="120" animated bg="#e4e4e4" />
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts" setup>
@@ -46,6 +60,22 @@ const props = defineProps<{
 
     .price {
       color: @priceColor;
+    }
+  }
+}
+
+.home-skeleton {
+  width: 1240px;
+  height: 406px;
+  display: flex;
+  justify-content: space-between;
+
+  .item {
+    width: 306px;
+
+    .xtx-skeleton ~ .xtx-skeleton {
+      display: block;
+      margin: 16px auto 0;
     }
   }
 }
