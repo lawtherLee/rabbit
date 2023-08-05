b95ae71144e0bba7d2a7d4819c7aaf03c6c61119
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-08-05 15:30:12 +0800

    feat:解决1前面是undefined的报错,渲染商品图片区域

--- src/store/modules/goods.ts
+++ src/store/modules/goods.ts
@@ -1,14 +1,18 @@
 import { defineStore } from "pinia";
+import request from "@/utils/request.ts";
+import { IAxiosRes } from "@/types/data";
+import { GoodsInfo } from "@/types/goods";
 
 export default defineStore("goods", {
   state: () => {
     return {
-      goods: {},
+      goods: {} as GoodsInfo,
     };
   },
   actions: {
-    getGoodsInfo(id: string) {
-      console.log("goodsDetail");
+    async getGoodsInfo(id: string) {
+      const res = await request.get<IAxiosRes<GoodsInfo>>("/goods?id=" + id);
+      this.goods = res.data.result;
     },
   },
 });
--- src/views/Goods/index.vue
+++ src/views/Goods/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useRoute } from "vue-router";
 import useStore from "@/store";
+import GoodsImages from "@/views/Goods/components/GoodsImges.vue";
 
 const route = useRoute();
 const { goodsStore } = useStore();
@@ -9,8 +10,45 @@ goodsStore.getGoodsInfo(route.params.goodsId as string);
 
 <template>
   <div class="goods-container">
-    <div class="container">123</div>
+    <!--    解决1前面是undefined的报错-->
+    <div v-if="goodsStore.goods.name" class="container">
+      <!--      面包屑-->
+      <xtx-bread>
+        <xtx-bread-item>首页</xtx-bread-item>
+        <xtx-bread-item
+          >{{ goodsStore.goods.categories[1].name }}
+        </xtx-bread-item>
+        <xtx-bread-item
+          >{{ goodsStore.goods.categories[0]?.name }}
+        </xtx-bread-item>
+        <xtx-bread-item>{{ goodsStore.goods.name }}</xtx-bread-item>
+      </xtx-bread>
+      <!-- 商品信息 -->
+      <div class="goods-info">
+        <div class="media">
+          <GoodsImages></GoodsImages>
+        </div>
+        <div class="spec"></div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="less" scoped>
+.goods-info {
+  min-height: 600px;
+  background: #fff;
+  display: flex;
+
+  .media {
+    width: 580px;
+    height: 600px;
+    padding: 30px 50px;
+  }
+
+  .spec {
+    flex: 1;
+    padding: 30px 30px 30px 0;
+  }
+}
+</style>
